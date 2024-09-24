3048fbc13e2f4985cb8142d429b21b5af5d39d60
nishant9144 <61697607+nishant9144@users.noreply.github.com>
2024-09-24 21:13:13 +0530

    Delete node.py

--- node.py
+++
@@ -1,9 +0,0 @@
-from object import *
-
-class Node:
-    def __init__(self,key, value=None):
-        self.left = None
-        self.right = None
-        self.key = key
-        self.value = value 
-        self.height = 1
